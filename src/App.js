5d560e4328200492ca4c0002d723c6ed1bd8c66b
John Orzechowski <johnorze@gmail.com>
2020-05-01 15:36:07 -0500

    5-1-20 commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ import { modifyPDF } from "./modifyPDF"
 import "jquery-ui/themes/base/all.css";
 import "nouislider/distribute/nouislider.css";
 import "select2/dist/css/select2.css";
+import "bootstrap-slider/dist/css/bootstrap-slider.css";
 
-import "jquery-bar-rating/dist/themes/css-stars.css";
 
 import $ from "jquery";
 import "jquery-ui/ui/widgets/datepicker.js";
 import "select2/dist/js/select2.js";
-import "jquery-bar-rating";
 
 import * as SurveyCore from "survey-core";
 import * as SurveyPDF from "survey-pdf";
